d5e1399f6d1632eb38456637e3f36699e85bc816
Edgar Cumbreras <edgarcumbrerastirado@gmail.com>
2019-05-19 00:28:27 +0200

    Include base client pulling data from an specific environment

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -12,67 +13,95 @@ import (
 	"google.golang.org/api/iterator"
 )
 
+type PubSubClient struct {
+	env    string
+	client *pubsub.Client
+}
+
 func main() {
+	env := flag.String("env", "staging", "Environment to pull events from")
+	flag.Parse()
+	pc := newPubSubClient(*env)
+	pc.start()
+
+}
+
+func (pc *PubSubClient) start() {
+	subs, err := pc.listSubscriptionsFromEnvironment()
+	if err != nil {
+		log.Fatal(err)
+	}
+	pc.printEventsFromSubscriptions(subs)
+}
+
+func (pc *PubSubClient) printEventsFromSubscriptions(subscriptions []*pubsub.Subscription) {
+	events := make(chan []byte)
+
+	for _, sub := range subscriptions {
+		go pc.readEvents(sub.ID(), events)
+	}
+
+	for e := range events {
+		fmt.Printf("Event received:\n %s\n", string(e))
+	}
+}
+
+func newPubSubClient(env string) PubSubClient {
 	ctx := context.Background()
 	proj := os.Getenv("GOOGLE_CLOUD_PROJECT")
+
 	if proj == "" {
 		fmt.Fprintf(os.Stderr, "GOOGLE_CLOUD_PROJECT environment variable must be set.\n")
 		os.Exit(1)
 	}
-	client, err := pubsub.NewClient(ctx, proj)
-	if err != nil {
-		log.Fatalf("Could not create pubsub Client: %v", err)
-	}
 
-	fmt.Println("Listing all subscriptions from the project:")
-	subs, err := list(client)
+	client, err := pubsub.NewClient(ctx, proj)
+
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	messages := make(chan string, 100)
-	for _, sub := range subs {
-		go pullMsgs(client, sub.ID(), messages)
-	}
-
-	for m := range messages {
-		fmt.Println("Messages %v", m)
-	}
+	return PubSubClient{client: client, env: env}
 }
 
-func list(client *pubsub.Client) ([]*pubsub.Subscription, error) {
+func (pc *PubSubClient) listSubscriptionsFromEnvironment() ([]*pubsub.Subscription, error) {
+	fmt.Println("Listing all subscriptions from the project:")
 	ctx := context.Background()
 	var subs []*pubsub.Subscription
-	it := client.Subscriptions(ctx)
+	it := pc.client.Subscriptions(ctx)
+
 	for {
 		s, err := it.Next()
+
 		if err == iterator.Done {
 			break
 		}
+
 		if err != nil {
 			return nil, err
 		}
 
-		if strings.Contains(s.ID(), "staging") {
-			fmt.Println("subscription found %v", s.ID())
+		if strings.Contains(s.ID(), pc.env) {
+			fmt.Printf("subscription found %s\n", s.ID())
 			subs = append(subs, s)
 		}
 	}
+
 	return subs, nil
 }
 
-func pullMsgs(client *pubsub.Client, subName string, messages <-chan string) error {
+func (pc *PubSubClient) readEvents(subscriptionName string, events chan<- []byte) {
 	ctx := context.Background()
 	var mu sync.Mutex
-	sub := client.Subscription(subName)
+	sub := pc.client.Subscription(subscriptionName)
 	cctx, _ := context.WithCancel(ctx)
 	err := sub.Receive(cctx, func(ctx context.Context, msg *pubsub.Message) {
-		fmt.Printf("Got message: %q\n", string(msg.Data))
 		mu.Lock()
+		events <- msg.Data
 		defer mu.Unlock()
 	})
+
 	if err != nil {
-		return err
+		panic(err)
 	}
-	return nil
 }
